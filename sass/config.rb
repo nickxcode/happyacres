5ceac42d309ef363cc6f1e60e5b3af4cef58bd6c
nickxcode <152753859+nickxcode@users.noreply.github.com>
2023-12-02 20:17:20 -0600

    Add files via upload

---
+++ sass/config.rb
@@ -0,0 +1,17 @@
+http_path = "/"
+css_dir = "../css"
+sass_dir = ""
+images_dir = "../img"
+javascripts_dir = "../js"
+
+
+preferred_syntax = :scss
+
+css_dir = "../css" # by Compass.app 
+sass_dir = "" # by Compass.app 
+images_dir = "../img" # by Compass.app 
+output_style = :expanded # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
